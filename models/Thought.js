19bca49b027082ebbb3d3fd8241b36ca66cd43c2
Corinne Marshall <corinnemarshall@Corinnes-MacBook-Air.local>
2021-11-29 15:13:57 -0500

    added thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -21,3 +21,36 @@ const ReactionSchema = new Schema({
         get: createdAtVal => formatDate(createdAtVal)
     }
 });
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280
+    },
+    createdAt: {
+        type: Date,
+        get: createdAtVal => formatDate(createdAtVal)
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [ReactionSchema]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+module.exports = Thought;
